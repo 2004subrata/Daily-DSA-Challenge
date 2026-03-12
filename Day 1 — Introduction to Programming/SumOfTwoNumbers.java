4853a7c74ada8cc2a6da55bdba6cab2ed9eb0270
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-03-12 23:18:27 +0530

    Day 1 — Introduction to Programming

---
+++ Day 1 — Introduction to Programming/SumOfTwoNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+
+        System.out.print("Sum of " + a + " and " + b + " is : " + (a+b));
+    }
+}
