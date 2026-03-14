c2db4e75962eef1ca68051f1a7aac5d4e8cd2f3f
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-03-14 23:54:24 +0530

    Add AreaOfTriangle class to calculate the area of a triangle

--- Day 3 — Loops & Conditional Statements/AreaOfCircle.java
+++ Day 3 — Loops & Conditional Statements/AreaOfCircle.java
@@ -1,5 +1,14 @@
 
 import java.util.Scanner;
+
 public class AreaOfCircle {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the radius : ");
+        float r = sc.nextFloat();
+
+        double area = Math.PI * r * r;
+        System.out.print("Area of the circle is : " + area);
+    }
 }
---
+++ Day 3 — Loops & Conditional Statements/AreaOfTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter base of triangle : ");
+        int base = sc.nextInt();
+
+        System.out.print("Enter height of triangle : ");
+        int height = sc.nextInt();
+
+        double area = 0.5 * base * height;
+        System.out.print("Area of the triangle : " + area);
+    }
+}
